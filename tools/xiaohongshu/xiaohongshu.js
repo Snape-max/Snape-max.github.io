06131a9010a3b0bb71152ff3a42a00167bbaa7c7
Snape-max <ssnape@qq.com>
2024-09-15 00:49:54 +0800

    xiaohongshu

--- tools/xiaohongshu/xiaohongshu.js
+++ tools/xiaohongshu/xiaohongshu.js
@@ -1,4 +1,5 @@
 api = "https://xiaohongshuapi1-z6j647l4.b4a.run/api/v1/image"
+api = "http://127.0.0.1:5000/api/v1/image"
 
 var picarea = document.getElementById("pic");
 var result = document.getElementById("result");
@@ -15,8 +16,8 @@ var webformat = "?imageView2/2/w/120/format/jpg";
 
 
 
-function Parser(type, link) {
-    const url = `${api}/${type}/${link}`;
+function Parser(link) {
+    const url = `${api}/${link}`;
     
     // 返回一个Promise，以便外部可以处理解析后的数据
     return fetch(url)
@@ -61,28 +62,9 @@ document.getElementById("btn").addEventListener('click', () => {
     var info = document.getElementById("info");
     var url = text.match(/(https?:\/\/|ftp:\/\/)[^\"'\s]+/g);
     console.log(url);
+    console.log(encodeURIComponent(url));
     if (url){
-        if (url.length > 0){
-            url = url[0];
-        }
-        
-        info.style.display = "none";
-        link = null;
-        type = null;
-        if (url.includes("xhslink")){
-            link = url.match(/xhslink\.com\/(.+)/)[1];
-            type = "short";
-        } else if (url.includes("xiaohongshu.com/explore")){
-            link = url.match(/explore\/([^?]+)/)[1];
-            type = "long";
-        } else if (url.includes("xiaohongshu.com/discovery/item/")) {
-          link = url.match(/item\/([^?]+)/)[1];
-          type = "long";
-        }
-
-        if (link){
-            var data = Parser(type, link);
-        }
+        Parser(encodeURIComponent(url));
     } else {
         info.style.display = "none";
     }
