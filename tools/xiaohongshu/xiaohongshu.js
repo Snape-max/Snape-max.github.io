f51a9e19011b0b08f914cdb11925352d3dbcbe51
Snape-max <ssnape@qq.com>
2025-02-16 18:21:58 +0800

    refactor(xiaohongshu): 重构小红书图片解析功能
    - 更新 API 地址为
    - 重构 Parser 函数，使用 async/await 替代 Promise 链
    - 优化代码结构，提高可读性和维护性
    - 添加 console.log 语句用于调试

--- tools/xiaohongshu/xiaohongshu.js
+++ tools/xiaohongshu/xiaohongshu.js
@@ -1,5 +1,4 @@
-api = "https://xiaohongshuapi1-z6j647l4.b4a.run/api/v1/image"
-// api = "http://127.0.0.1:5000/api/v1/image"
+api = "https://api.qiumo.fun/xiaohongshu/image/?url="
 
 var picarea = document.getElementById("pic");
 var result = document.getElementById("result");
@@ -17,49 +16,43 @@ var webformat = "?imageView2/2/w/120/format/jpg";
 
 
 
-function Parser(link) {
+async function Parser(link) {
     result.style.display = "none";
     info.innerHTML = `<h2>正在解析...</h2>`;
-    const url = `${api}/${link}`;
-    
+    const url = `${api}${link}`;
+    // console.log(url);
     // 返回一个Promise，以便外部可以处理解析后的数据
-    return fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          info.innerHTML = `<h2>网络错误</h2>`;
-          throw new Error(`HTTP 错误：${response.status}`);
-        }
-        // 使用return关键字返回解析后的JSON数据
-        return response.json();
-      })
-      .then((data) => {
-        // 确保数据中有images属性再返回
-        if (data && data.images) {
-            info.innerHTML = "";
-            result.style.display = "block";
-            console.log(data.images);
-            console.log(data.description);
-            imgHtml = "";
-            for (let i=0;i<data.images.length;i++){
-                // imgHtml += `<a href='${data.images[i]}' download>图片${i+1}, 点击下载</a>`;
-                imgHtml += 
-                `<a href='${ci + data.images[i] + rawformat}' target='_blank'><img src='${ci + data.images[i] + webformat}' href='${data.images[i]}'></a>`;
-            }
+    try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      info.innerHTML = `<h2>网络错误</h2>`;
+      throw new Error(`HTTP 错误：${response.status}`);
+    }
+    const data = await response.json();
+    // 确保数据中有images属性再返回
+    if (data && data.images) {
+      info.innerHTML = "";
+      result.style.display = "block";
+      imgHtml = "";
+      for (let i = 0; i < data.images.length; i++) {
+        // imgHtml += `<a href='${data.images[i]}' download>图片${i+1}, 点击下载</a>`;
+        imgHtml +=
+          `<a href='${ci + data.images[i] + rawformat}' target='_blank'><img src='${ci + data.images[i] + webformat}' href='${data.images[i]}'></a>`;
+      }
 
-            picarea.innerHTML =  imgHtml;
-            description.innerHTML = `<h2>文案</h2><p>${data.description}</p>`
+      picarea.innerHTML = imgHtml;
+      description.innerHTML = `<h2>文案</h2><p>${data.description}</p>`;
 
 
-        } else {
-          info.innerHTML = `<h2>未找到图片</h2>`;
-          throw new Error('No images found in the API response');
-        }
-      })
-      .catch((error) => {
-        info.innerHTML = `<h2>解析失败</h2>`;
-        console.error('There was an error fetching the data:', error);
-        throw error; // 抛出错误以在外部捕获
-      });
+    } else {
+      info.innerHTML = `<h2>未找到图片</h2>`;
+      throw new Error('No images found in the API response');
+    }
+  } catch (error) {
+    info.innerHTML = `<h2>解析失败</h2>`;
+    console.error('There was an error fetching the data:', error);
+    throw error; // 抛出错误以在外部捕获
+  }
 }
 
 
@@ -67,8 +60,8 @@ document.getElementById("btn").addEventListener('click', () => {
     var text = document.getElementById("data").value;
     
     var url = text.match(/(https?:\/\/|ftp:\/\/)[^\"'\s]+/g);
-    console.log(url);
-    console.log(encodeURIComponent(url));
+    // console.log(url);
+    // console.log(encodeURIComponent(url));
     if (url){
         Parser(encodeURIComponent(url));
     } else {
